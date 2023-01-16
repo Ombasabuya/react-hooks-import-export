64d2fecd6a83f91ff241a838efd100255f0f8800
ombasaabuya <ombasabuya@gmail.com>
2023-01-16 13:48:55 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
 import ColoradoStateParks from "./ColoradoStateParks";
-
-ReactDOM.render(<ColoradoStateParks />, document.getElementById("root"));
+// ReactDOM.render(<ColoradoStateParks />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
